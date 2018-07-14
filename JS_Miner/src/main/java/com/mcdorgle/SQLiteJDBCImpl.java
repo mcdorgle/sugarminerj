02e988c167b4d24ecf4ce1d9c2abdfab98d3783c
arisance <2018609d@opayq.com>
2018-07-14 13:00:39 -0500

    Refactor to include a package and add a gradle build

--- JS_Miner/src/main/java/com/mcdorgle/SQLiteJDBCImpl.java
+++ JS_Miner/src/main/java/com/mcdorgle/SQLiteJDBCImpl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package com.mcdorgle;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
